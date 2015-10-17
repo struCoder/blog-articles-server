1f937e2aad1308b993e01f2bb0e79f3d05882622
strucoder <zdwloveschina@163.com>
2015-10-17 18:21:59 +0800

    finish

--- index.js
+++ index.js
@@ -1,40 +1,56 @@
 var http = require('http');
 var serveStatic = require('serve-static');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 var express = require('express');
 var crypto = require('crypto');
-var bl = require('bl');
 var secret = require('./config').secret;
-console.log(secret)
 var log = require('util').log;
 
 var app = express();
 var port = 9002;
 
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(serveStatic('public/articles', {'index': ['index.html', 'index.htm']}));
+app.use(serveStatic('public/articles', {
+  'index': ['index.html', 'index.htm']
+}));
+
+
 app.post('/release-new-version', function(req, res) {
-  var body = req.body;
-  var sig = 'sha1=' + crypto.createHmac('sha1', secret).update(JSON.stringify(body)).digest('hex');
-  if (req.headers['x-hub-signature'] && sig === req.headers['x-hub-signature']) {
-    log('github webhooks', body);
-    var exec = require('child_process').exec
-    exec('sh gen.sh', function(err, stdout,stderr) {
-      if (err) {
-        log('error:', err)
-      } else {
-        log('stdout: ', stdout);
-        res.json({code: 0, msg: 'success update'});
-      }
-    });
-  } else {
-    res.statusCode = 403;
-    res.json({code: 1000, msg: 'forbidden'});
-  }
+  var bufferArr = [];
+  var bufferLen = 0;
+  req.on('data', function(chunk) {
+    bufferArr.push(chunk);
+    bufferLen += chunk.length;
+  });
+
+  req.on('end', function(chunk) {
+    if (chunk) {
+      bufferArr.push(chunk);
+      bufferLen += chunk.length;
+    }
+    var data = Buffer.concat(bufferArr, bufferLen).toString();
+    var sig = 'sha1=' + crypto.createHmac('sha1', secret).update(data).digest('hex');
+    if (req.headers['x-hub-signature'] && sig === req.headers['x-hub-signature']) {
+      log('github webhooks');
+      var exec = require('child_process').exec
+      exec('sh gen.sh', function(err, stdout, stderr) {
+        if (err) {
+          log('error:', err)
+        } else {
+          log('stdout: ', stdout);
+          res.json({
+            code: 0,
+            msg: 'success update'
+          });
+        }
+      });
+    } else {
+      res.statusCode = 403;
+      res.json({
+        code: 1000,
+        msg: 'forbidden'
+      });
+    }
+  })
 });
 
 // Create server
